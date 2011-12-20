e39aacdb9db157ba9e96543bdf7771475c74de3a
Matt Kane <mkb-prime@hydrogenproject.com>
2011-12-20 16:56:17 -0500

    stuff

--- src/echonest/core.go
+++ src/echonest/core.go
@@ -19,11 +19,20 @@ type Echonest struct {
 	Host string
 }
 
+type Arg struct {
+	Key, Value string
+}
+
+// return url-escape key=value string
+func (a *Arg) Joined() string {
+	return fmt.Sprintf("%s=%s", a.Key, url.QueryEscape(a.Value))
+}
+
 func init() {
 	for _, r := range os.Environ() {
 		if strings.HasPrefix(r, "ECHO_NEST_API_KEY") {
 			split := strings.SplitN(r, "=", 2)
-			key = split[1]
+			defaultkey = split[1]
 			break
 		}
 	}
@@ -45,17 +54,15 @@ func New() *Echonest {
 	return o
 }
 
-func (e Echonest) GetCall(path string, method string, stringargs map[string]string, floatargs map[string]float64, intargs map[string]int) http.Response, error {
-	args := make([]string, 0, len(stringargs) + len(floatargs) + len(intargs) + 1) // +1 for api key
-	for k, v := range stringargs {
-		args = append(args, url.QueryEscape(k + "=" + v))
+func (e *Echonest) keyArg() string {
+	return fmt.Sprintf("api_key=%s", e.Key)
+}
+
+func (e *Echonest) GetCall(path string, method string, inargs []*Arg) (*http.Response, error) {
+	args := make([]string, 0, len(inargs) + 1) // +1 for api key
+	for _, r := range inargs {
+		args = append(args, r.Joined())
 	}
-	for k, v := range intargs {
-		args = append(args, url.QueryEscape(fmt.Sprintf("%s=%d", v)))
-	}
-	for k, v := range floatargs {
-		args = append(args, url.QueryEscape(fmt.Sprintf("%s=%f", v)))
-	}
-	
-	return Get(strings.Join([]string{"http:/", e.Host, basepath, path, method}, "/") + "?" + strings.Join(args, "/"))
+	args = append(args, e.keyArg())
+	return http.Get(strings.Join([]string{"http:/", e.Host, basepath, path, method}, "/") + "?" + strings.Join(args, "&"))
 }
--- src/echonest/song.go
+++ src/echonest/song.go
@@ -1,7 +1,56 @@
 package echonest
 
-import "fmt"
+//import "fmt"
+import "encoding/json"
 
-func (e Echonest) SongSearch(stringargs map[string]string, floatargs map[string]float64, intargs map[string]int) error {
-	return genericMethod
+type Status struct {
+	version string
+	code int
+	message string
+}
+
+type AudioSummary_t struct {
+	Duration float64
+	Energy float64
+	Mode bool
+	AnalysisUrl string `json:"analysis_url"`
+	TimeSignature int `json:"time_signature"`
+	Key int
+	Loudness float64
+	AudioMd5 string `json:"audio_md5"`
+	Tempo float64
+	Danceability float64
+}
+
+type Track struct {
+	ForeignReleaseId string `json:"foreign_release_id"`
+	PreviewUrl string `json:"preview_url"`
+	ReleaseImage string `json:"release_image"`
+	Catalog string
+	ForeignId string `json:"foreign_release_id"`
+}
+
+type Song struct {
+	AudioMd5 string `json:"audio_md5"`
+	Id string 
+	Title string
+	ArtistName string `json:"artist_name"`
+	AudioSummary AudioSummary_t `json:"audio_summary"`
+	ArtistId string `json:"artist_id"`
+	Tracks []Track
+}
+	
+func (e *Echonest) SongSearch(args []*Arg) (songs []Song, err error) {
+	r, err := e.GetCall("song", "search", args)
+	if err != nil {
+		return
+	}
+	defer r.Body.Close()
+	m := make(map[string]interface{})
+	// m["response"] = make(map[string]interface{})
+	// m["response"]["status"] = new(Status)
+	// m["response"]["songs"] = make([]Song, 0)
+	jd := json.NewDecoder(r.Body)
+	err = jd.Decode(&m)
+	return
 }
